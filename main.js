243b7696049a8a7adf53d5563db3c157a9edadec
hdrsy <126724238+hdrsy@users.noreply.github.com>
2023-03-13 16:04:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// let services = document.querySelector('#secHomeServ');
+
+
+
+// let nav = document.querySelector('.fullNavi');
+// let li = document.querySelectorAll('.fullNavi a');
+// nav.addEventListener('click', () => {
+// s
+// })
+// console.log(nav)
+// console.log(typeof li)
+// // nav.addEventListener('click' , () => {
+// //     console.log(nav);
+// // })
+
+
+// li.forEach((e) => {
+//     console.log(console.log(e))
+// })
